8f80e772c7485f3633bdcf4c1ae657d83185b405
OonisiHiroki <k021g1328@g.neec.ac.jp>
2022-11-07 15:23:18 +0900

    02_04_テクスチャ付き四角形

--- Resources/shaders/BasicGeometryShader.hlsl
+++ Resources/shaders/BasicGeometryShader.hlsl
@@ -1,5 +1,23 @@
 #include "BasicShaderHeader.hlsli"
 
+//四角形の頂点数
+static const uint vnum = 4;
+
+//センターからのオフセット
+static const float4 offset_array[vnum] = {
+	float4(-0.5f, -0.5f, 0, 0),	//左下
+	float4(-0.5f, +0.5f, 0, 0),	//左上
+	float4(+0.5f, -0.5f, 0, 0),	//右下
+	float4(+0.5f, +0.5f, 0, 0)	//右上
+};
+
+//UVの割り当て
+static const float2 uv_array[vnum] = {
+	float2(0,1),
+	float2(0,0),
+	float2(1,1),
+	float2(1,0)
+};
 //struct VSOutput
 //{
 //	float4 pos : SV_POSITION;
@@ -72,24 +90,20 @@
 //}
 
 //三角形の出力
-[maxvertexcount(3)]
+[maxvertexcount(vnum)]
 void main(
 	point VSOutput input[1] : SV_POSITION,
 	inout TriangleStream< GSOutput > output
 ) {
 	GSOutput element;
-	//共通
-	element.normal = input[0].normal;
-	element.uv = input[0].uv;
-
-	//1点目
-	element.svpos = input[0].svpos;
-	output.Append(element);
-
-	//2点目
-	element.svpos = input[0].svpos + float4(10.0f, 10.0f, 0, 0);
-	output.Append(element);
-	//3点目
-	element.svpos = input[0].svpos + float4(10.0f, 0, 0, 0);
-	output.Append(element);
+	//4点分まわす
+	for (uint i = 0; i < vnum; i++) {
+		//ワールド座標ベースで、ずらす
+		element.svpos = input[0].pos + offset_array[i];
+		//ビュー、射影変換
+		element.svpos = mul(mat, element.svpos);
+		//element.uv = float2(0.5f, 0.5f);
+		element.uv = uv_array[i];
+		output.Append(element);
+	}
 }
